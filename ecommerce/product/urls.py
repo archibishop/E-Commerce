3f88bd1cf8e612412d2a66a3d16b335b90e27734
Wagubi Brian <wagubib@gmail.com>
2019-07-15 20:40:59 +0300

    feat(category): View Products based of the category
     - User should be able to see products by a certain category
    
    [Delivers #167280110]

--- ecommerce/product/urls.py
+++ ecommerce/product/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('list', views.ProductListView.as_view(), name='product-list'),
     path('vendor/<int:id>', views.ProductsVendorListView.as_view(),
          name='products-vendor'),
+    path('category/<str:category_name>', views.ProductsCategoryListView.as_view(),
+         name='products-category'),
 ]
 
 app_name = 'product'
--- ecommerce/product/views.py
+++ ecommerce/product/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Category
 from django.views.generic.list import ListView
 from authentication.models import Person
 
@@ -12,6 +12,7 @@ class ProductListView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['vendors'] = get_vendors()
+        context['categories'] = get_categories()
         return context
 
 
@@ -26,9 +27,30 @@ class ProductsVendorListView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['vendors'] = get_vendors()
+        context['categories'] = get_categories()
+        return context
+
+
+class ProductsCategoryListView(ListView):
+    model = Product
+    template_name = 'product_list.html'
+
+    def get_queryset(self):
+        queryset = Product.objects.filter(category__category_name=self.kwargs['category_name'])
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['vendors'] = get_vendors()
+        context['categories'] = get_categories()
         return context
 
 
 def get_vendors():
     vendors = Person.objects.filter(customer=False)
     return vendors
+
+
+def get_categories():
+    categories = Category.objects.all()
+    return categories
