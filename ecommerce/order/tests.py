1ccf7d7c4bde37322ed0fa03df2ad85775841b97
Wagubi Brian <36948493+archibishop@users.noreply.github.com>
2019-07-22 12:03:44 +0300

    feat(history): User should view their order history (#11)
    - Return order for a particular user
    
    [Delivers #167280128

--- ecommerce/order/tests.py
+++ ecommerce/order/tests.py
@@ -33,3 +33,34 @@ class OrdersTestCase(TestCase):
         orders = Orders.objects.all()
         self.assertEqual(len(orders), 1)
         self.assertEqual(response.status_code, 302)
+
+    def test_order_history_page_displays(self):
+        client = Client()
+        user = User.objects.create_user(username='user_name',
+                                        email='email',
+                                        password='password',
+                                        first_name='first_name',
+                                        last_name='last_name')
+        client.login(username='user_name', password='password')
+        response = client.get('/product/list')
+        response = client.post('/product/cart', {'product_id': 1,
+                                                 'product_name': "Airmax shoes",
+                                                 'product_price': "9000",
+                                                 'product_category': "shoes"},
+                               HTTP_REFERER='http://www.google.com')
+        session = client.session
+        self.assertEqual(len(session['selected_items']), 1)
+        response = client.post(reverse('order:order-products'),
+                               {'quantity': 2}, HTTP_REFERER='http://www.google.com')
+        orders = Orders.objects.all()
+        self.assertEqual(len(orders), 1)
+        self.assertEqual(response.status_code, 302)
+        response = client.get(reverse('order:order-history'))
+        self.assertContains(
+            response, 'Download PDF', status_code=200)
+
+    def test_order_history_page_displays_no_orders(self):
+        client = Client()
+        response = client.get(reverse('order:order-history'))
+        self.assertContains(
+            response, 'No Orders', status_code=200)
--- ecommerce/order/urls.py
+++ ecommerce/order/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('order', views.OrderView.as_view(), name='order-products'),
+    path('history', views.OrderHistoryView.as_view(), name='order-history'),
 ]
 
 app_name = 'order'
