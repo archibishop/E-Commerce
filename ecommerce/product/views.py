a2806710827a2ec6359dc8a7b8e9517ac4a11fd9
Wagubi Brian <36948493+archibishop@users.noreply.github.com>
2019-07-22 10:24:38 +0300

    feat(vendors): View products from specific vendors (#6)
    - A user should be able to view products from a specific vendor
    
    [Delivers #167280100]

--- ecommerce/product/views.py
+++ ecommerce/product/views.py
@@ -1,9 +1,34 @@
 from django.shortcuts import render
 from .models import Product
 from django.views.generic.list import ListView
+from authentication.models import Person
 
 # Create your views here.
 
 class ProductListView(ListView):
     model = Product
     template_name = 'product_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['vendors'] = get_vendors()
+        return context
+
+
+class ProductsVendorListView(ListView):
+    model = Product
+    template_name = 'product_list.html'
+
+    def get_queryset(self):
+        queryset = Product.objects.filter(user_id=self.kwargs['id'])
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['vendors'] = get_vendors()
+        return context
+
+
+def get_vendors():
+    vendors = Person.objects.filter(customer=False)
+    return vendors
